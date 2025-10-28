4f03161e0368ecb0c77016a2273d1d64910b7a2b
Hun Jung <jsh@robotis.com>
2025-10-28 10:10:46 +0900

    Added default argument
    Signed-off-by: Hun Jung <jsh@robotis.com>

--- detectron2/engine/defaults.py
+++ detectron2/engine/defaults.py
@@ -117,6 +117,7 @@ Run on multiple machines:
     )
     parser.add_argument("--eval-only", action="store_true", help="perform evaluation only")
     parser.add_argument("--num-gpus", type=int, default=1, help="number of gpus *per machine*")
+    parser.add_argument("--custom", default=True, action='store_true', help="use custom dataset")
     parser.add_argument("--num-machines", type=int, default=1, help="total number of machines")
     parser.add_argument(
         "--machine-rank", type=int, default=0, help="the rank of this machine (unique per machine)"
